153ef0b10cde363aac7d5336e23ce8c1007e2b48
Kolomiiets Lidiia <kolomieclidia7@gmail.com>
2023-06-11 21:50:02 +0300

    fix comments

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-let result = '';
+
 
 const charactersLength = characters.length;
 
 function generateKey(length, characters){
+    let result = '';
+
     for(let i = 0; i < length; i++){
         result += characters.charAt(Math.random()*charactersLength)
     }
+
     return result
 }
 
 const key = generateKey(6, characters);
 console.log(key);
-
